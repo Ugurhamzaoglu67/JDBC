01dfc7245e2c4a9a8f96d2db94e44ee306bd6321
Ugur hamzaoglu <craxx67@gmail.com>
2021-09-13 18:22:19 +0300

    DB connection success

--- Database/src/com/ugurhmz/jdbc/ConnectionTest.java
+++ Database/src/com/ugurhmz/jdbc/ConnectionTest.java
@@ -1,5 +1,33 @@
 package com.ugurhmz.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionTest {
 
+	public static void main(String[] args) throws ClassNotFoundException {
+		
+		
+		Class.forName("com.mysql.jdbc.Driver"); 
+		
+		String url = "jdbc:mysql://localhost:3306/jdbcexample";
+		String userName = "root";
+		String password= "1994ugur";
+		
+		
+		try {
+			Connection connection = DriverManager.getConnection(url,userName,password);
+			
+			System.out.println("DB name : " + connection.getMetaData().getDatabaseProductName());
+			
+			
+			
+			connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 }
