e5d16ed97163d093f4d4237c7c87031e9ff14e21
Ugur hamzaoglu <craxx67@gmail.com>
2021-09-14 08:03:17 +0300

    Safe Insert

--- Database/src/com/ugurhmz/jdbc/insert/InsertJdbc.java
+++ Database/src/com/ugurhmz/jdbc/insert/InsertJdbc.java
@@ -2,6 +2,7 @@ package com.ugurhmz.jdbc.insert;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -11,25 +12,31 @@ public class InsertJdbc {
 		
 		Class.forName("com.mysql.jdbc.Driver");
 		
-		String url = "jdbc:mysql://localhost:3306/jdbcexample";
-		String name = "root";
-		String password = "1994ugur";
+		String url="jdbc:mysql://localhost:3306/jdbcexample";
+		String dbName = "root";
+		String  password="1994ugur";
 		
 		try {
-			Connection connection = DriverManager.getConnection(url,name,password);
+			Connection connection = DriverManager.getConnection(url,dbName, password);
+			String sql = "INSERT INTO Product(productName,salesPrice) VALUES(?,?)";
+			PreparedStatement statement = connection.prepareStatement(sql);
 			
-			String insertSql = "INSERT INTO Product(productName,salesPrice) values('Iphone 7s',500)";
-			Statement statement = connection.createStatement();
+			statement.setString(1, "LCD TV");
+			statement.setDouble(2, 800);
+			
+			int affected = statement.executeUpdate();
+			
+			System.out.println("affected : "+affected);
 			
 			
-			int affected = statement.executeUpdate(insertSql);
-			
-			
-			System.out.println("insert successfull, affected row : "+affected);
 			connection.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
+	
+		
 	}
 }
